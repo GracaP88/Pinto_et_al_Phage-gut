f1572209cbb2dfc3ffbb48091eab04ca63f56744
GracaP88 <65946642+GracaP88@users.noreply.github.com>
2022-01-05 09:59:55 +0000

    Add files via upload

---
+++ Analysis/structure.R
@@ -0,0 +1,14 @@
+dir.create("data")
+dir.create("data/rds")
+
+dir.create("01_wetlab")
+dir.create("01_wetlab/figs")
+
+dir.create("02_composition")
+dir.create("02_composition/figs")
+
+dir.create("03_pathways")
+dir.create("03_pathways/figs")
+
+dir.create("04_phage")
+dir.create("04_phage/figs")
